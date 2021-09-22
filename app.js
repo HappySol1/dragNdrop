6895758d7bb7391e1591ff997a02bf151eee2896
HappySol1 <dimon53534@gmail.com>
2021-09-22 23:42:57 +0300

    a

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
 item.addEventListener('dragstart',dragstart)
 item.addEventListener('dragend',dragend)
-console.log(1)
-function dragstart(){
-    console.log(1)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover',dragover)
+    placeholder.addEventListener('dragenter',dragenter)
+    placeholder.addEventListener('dragleave',dragleave)
+    placeholder.addEventListener('drop',dragdrop)
 }
 
-function dragend(){
-    console.log(2)
+function dragover(e){
+    e.preventDefault()
+}
+
+function dragenter(e){
+    e.target.classList.add('entered')
+}
+
+function dragleave(e){
+    e.target.classList.remove('entered')
+}
+
+function dragdrop(e){
+    e.target.append(item)
+    e.target.classList.remove('entered')
+}
+
+function dragstart(e){
+    e.target.classList.add('hold')
+    setTimeout(()=>e.target.classList.add('hide'),0)
+}
+
+function dragend(e){
+    e.target.className = 'item'
 }
